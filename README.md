d6fcd850a3461883dccc7746d654fbcb980ed907
Sandro Sodré Nunes <86071326+sandrosodrenunes@users.noreply.github.com>
2023-09-13 20:31:05 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para treinamento de programação.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home (){
-return <h1>Oi Sandro Sodré Junior</h1>
+function Home() {
+  return <h1>Oi Sandro Sodré Junior</h1>;
 }
 
 export default Home;
