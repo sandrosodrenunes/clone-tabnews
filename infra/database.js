bb059997c0694539bfc8269c9beb07454c9ac7e7
Sandro Sodré Nunes <86071326+sandrosodrenunes@users.noreply.github.com>
2024-07-25 19:42:51 +0000

    handel database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   console.log("Credenciais do Postgres: ", {
     host: process.env.POSTGRES_HOST,
@@ -21,7 +22,7 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console(error(error));
+    console.log(error);
     throw error;
   } finally {
     await client.end();
