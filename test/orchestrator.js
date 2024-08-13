1beef5f12658c7d37d07a08bc3e3a97ddc1ce4b2
Sandro Sodré Nunes <86071326+sandrosodrenunes@users.noreply.github.com>
2024-08-13 17:52:41 +0000

    new commit

--- test/orchestrator.js
+++ test/orchestrator.js
@@ -12,7 +12,7 @@ async function waitForAllServices() {
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
       const responseBody = await response.json();
-      console.log(responseBody);
+
       if (response.status !== 200) {
         throw Error();
       }
